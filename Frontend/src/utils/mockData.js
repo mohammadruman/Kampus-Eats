d736bb1fd024a74e1349b2a080db788a54f43bf5
mohammadruman <mohammaderuman@gmail.com>
2024-09-09 21:39:37 +0530

    update: data in organized and comprehensive format

--- Frontend/src/utils/mockData.js
+++ Frontend/src/utils/mockData.js
@@ -2,67 +2,350 @@ export const restaurants = [
   {
     name: "Foodcourt 1",
     campusName: "Campus 10",
-    cuisine: "Dosa, Idli, Sambhar, Vada, Sambar Rice",
-    rating: "4.5",
-    deliveryTime: "10",
-    price: "200",
+    overallRating: 4.5,
+    deliveryTime: 10, // in minutes
+    averagePrice: 200, // calculated average of all cuisine prices
+    cuisines: [
+      { 
+        name: "Dosa", 
+        price: 50, 
+        image: "url/to/dosa-image.jpg", 
+        rating: 4.6 
+      },
+      { 
+        name: "Idli", 
+        price: 30, 
+        image: "url/to/idli-image.jpg", 
+        rating: 4.4 
+      },
+      { 
+        name: "Sambhar", 
+        price: 40, 
+        image: "url/to/sambhar-image.jpg", 
+        rating: 4.5 
+      },
+      { 
+        name: "Vada", 
+        price: 20, 
+        image: "url/to/vada-image.jpg", 
+        rating: 4.3 
+      },
+      { 
+        name: "Sambar Rice", 
+        price: 60, 
+        image: "url/to/sambar-rice-image.jpg", 
+        rating: 4.7 
+      }
+    ]
   },
   {
     name: "Foodcourt 2",
     campusName: "Campus 13",
-    cuisine: "Pizza, Pasta, Garlic Bread, Salad",
-    rating: "4.0",
-    deliveryTime: "15",
-    price: "350",
+    overallRating: 4.0,
+    deliveryTime: 15,
+    averagePrice: 350,
+    cuisines: [
+      { 
+        name: "Pizza", 
+        price: 150, 
+        image: "url/to/pizza-image.jpg", 
+        rating: 4.3 
+      },
+      { 
+        name: "Pasta", 
+        price: 120, 
+        image: "url/to/pasta-image.jpg", 
+        rating: 4.0 
+      },
+      { 
+        name: "Garlic Bread", 
+        price: 70, 
+        image: "url/to/garlic-bread-image.jpg", 
+        rating: 4.1 
+      },
+      { 
+        name: "Salad", 
+        price: 50, 
+        image: "url/to/salad-image.jpg", 
+        rating: 4.0 
+      }
+    ]
   },
   {
     name: "Foodcourt 3",
     campusName: "Campus 2",
-    cuisine: "Burgers, Fries, Shakes, Onion Rings",
-    rating: "4.7",
-    deliveryTime: "12",
-    price: "250",
+    overallRating: 4.7,
+    deliveryTime: 12,
+    averagePrice: 250,
+    cuisines: [
+      { 
+        name: "Burgers", 
+        price: 120, 
+        image: "url/to/burger-image.jpg", 
+        rating: 4.8 
+      },
+      { 
+        name: "Fries", 
+        price: 80, 
+        image: "url/to/fries-image.jpg", 
+        rating: 4.6 
+      },
+      { 
+        name: "Shakes", 
+        price: 100, 
+        image: "url/to/shakes-image.jpg", 
+        rating: 4.7 
+      },
+      { 
+        name: "Onion Rings", 
+        price: 50, 
+        image: "url/to/onion-rings-image.jpg", 
+        rating: 4.5 
+      }
+    ]
   },
   {
     name: "Foodcourt 4",
     campusName: "Campus 5",
-    cuisine: "Indian Thali, Raita, Chapati, Pickle",
-    rating: "4.6",
-    deliveryTime: "18",
-    price: "300",
+    overallRating: 4.6,
+    deliveryTime: 18,
+    averagePrice: 300,
+    cuisines: [
+      { 
+        name: "Indian Thali", 
+        price: 200, 
+        image: "url/to/thali-image.jpg", 
+        rating: 4.7 
+      },
+      { 
+        name: "Raita", 
+        price: 40, 
+        image: "url/to/raita-image.jpg", 
+        rating: 4.4 
+      },
+      { 
+        name: "Chapati", 
+        price: 30, 
+        image: "url/to/chapati-image.jpg", 
+        rating: 4.5 
+      },
+      { 
+        name: "Pickle", 
+        price: 10, 
+        image: "url/to/pickle-image.jpg", 
+        rating: 4.3 
+      }
+    ]
   },
   {
     name: "Foodcourt 5",
     campusName: "Campus 9",
-    cuisine: "Noodles, Momos, Spring Rolls, Fried Rice",
-    rating: "4.3",
-    deliveryTime: "10",
-    price: "150",
+    overallRating: 4.3,
+    deliveryTime: 10,
+    averagePrice: 150,
+    cuisines: [
+      { 
+        name: "Noodles", 
+        price: 90, 
+        image: "url/to/noodles-image.jpg", 
+        rating: 4.2 
+      },
+      { 
+        name: "Momos", 
+        price: 60, 
+        image: "url/to/momos-image.jpg", 
+        rating: 4.3 
+      },
+      { 
+        name: "Spring Rolls", 
+        price: 80, 
+        image: "url/to/spring-rolls-image.jpg", 
+        rating: 4.1 
+      },
+      { 
+        name: "Fried Rice", 
+        price: 70, 
+        image: "url/to/fried-rice-image.jpg", 
+        rating: 4.2 
+      }
+    ]
   },
   {
     name: "Foodcourt 6",
     campusName: "Campus 12",
-    cuisine: "Wraps, Sandwiches, Smoothies, Fries",
-    rating: "4.2",
-    deliveryTime: "8",
-    price: "180",
+    overallRating: 4.2,
+    deliveryTime: 8,
+    averagePrice: 180,
+    cuisines: [
+      { 
+        name: "Wraps", 
+        price: 80, 
+        image: "url/to/wraps-image.jpg", 
+        rating: 4.3 
+      },
+      { 
+        name: "Sandwiches", 
+        price: 70, 
+        image: "url/to/sandwich-image.jpg", 
+        rating: 4.2 
+      },
+      { 
+        name: "Smoothies", 
+        price: 100, 
+        image: "url/to/smoothie-image.jpg", 
+        rating: 4.1 
+      },
+      { 
+        name: "Fries", 
+        price: 50, 
+        image: "url/to/fries-image.jpg", 
+        rating: 4.0 
+      }
+    ]
   },
-  
   {
     name: "Foodcourt 7",
     campusName: "Campus 18",
-    cuisine: "Sushi, Ramen, Tempura, Miso Soup",
-    rating: "4.8",
-    deliveryTime: "20",
-    price: "400",
+    overallRating: 4.8,
+    deliveryTime: 20,
+    averagePrice: 400,
+    cuisines: [
+      { 
+        name: "Sushi", 
+        price: 200, 
+        image: "url/to/sushi-image.jpg", 
+        rating: 4.9 
+      },
+      { 
+        name: "Ramen", 
+        price: 180, 
+        image: "url/to/ramen-image.jpg", 
+        rating: 4.8 
+      },
+      { 
+        name: "Tempura", 
+        price: 150, 
+        image: "url/to/tempura-image.jpg", 
+        rating: 4.7 
+      },
+      { 
+        name: "Miso Soup", 
+        price: 70, 
+        image: "url/to/miso-soup-image.jpg", 
+        rating: 4.5 
+      }
+    ]
   },
   {
     name: "Foodcourt 8",
     campusName: "Campus 6",
-    cuisine: "Burritos, Tacos, Quesadillas, Nachos",
-    rating: "4.4",
-    deliveryTime: "15",
-    price: "250",
-  },
- 
+    overallRating: 4.4,
+    deliveryTime: 15,
+    averagePrice: 250,
+    cuisines: [
+      { 
+        name: "Burritos", 
+        price: 120, 
+        image: "url/to/burritos-image.jpg", 
+        rating: 4.4 
+      },
+      { 
+        name: "Tacos", 
+        price: 100, 
+        image: "url/to/tacos-image.jpg", 
+        rating: 4.5 
+      },
+      { 
+        name: "Quesadillas", 
+        price: 110, 
+        image: "url/to/quesadillas-image.jpg", 
+        rating: 4.3 
+      },
+      { 
+        name: "Nachos", 
+        price: 90, 
+        image: "url/to/nachos-image.jpg", 
+        rating: 4.4 
+      }
+    ]
+  }
 ];
+
+
+
+
+
+
+
+
+
+// export const restaurants = [
+//   {
+//     name: "Foodcourt 1",
+//     campusName: "Campus 10",
+//     cuisine: "Dosa, Idli, Sambhar, Vada, Sambar Rice",
+//     rating: "4.5",
+//     deliveryTime: "10",
+//     price: "200",
+//   },
+//   {
+//     name: "Foodcourt 2",
+//     campusName: "Campus 13",
+//     cuisine: "Pizza, Pasta, Garlic Bread, Salad",
+//     rating: "4.0",
+//     deliveryTime: "15",
+//     price: "350",
+//   },
+//   {
+//     name: "Foodcourt 3",
+//     campusName: "Campus 2",
+//     cuisine: "Burgers, Fries, Shakes, Onion Rings",
+//     rating: "4.7",
+//     deliveryTime: "12",
+//     price: "250",
+//   },
+//   {
+//     name: "Foodcourt 4",
+//     campusName: "Campus 5",
+//     cuisine: "Indian Thali, Raita, Chapati, Pickle",
+//     rating: "4.6",
+//     deliveryTime: "18",
+//     price: "300",
+//   },
+//   {
+//     name: "Foodcourt 5",
+//     campusName: "Campus 9",
+//     cuisine: "Noodles, Momos, Spring Rolls, Fried Rice",
+//     rating: "4.3",
+//     deliveryTime: "10",
+//     price: "150",
+//   },
+//   {
+//     name: "Foodcourt 6",
+//     campusName: "Campus 12",
+//     cuisine: "Wraps, Sandwiches, Smoothies, Fries",
+//     rating: "4.2",
+//     deliveryTime: "8",
+//     price: "180",
+//   },
+  
+//   {
+//     name: "Foodcourt 7",
+//     campusName: "Campus 18",
+//     cuisine: "Sushi, Ramen, Tempura, Miso Soup",
+//     rating: "4.8",
+//     deliveryTime: "20",
+//     price: "400",
+//   },
+//   {
+//     name: "Foodcourt 8",
+//     campusName: "Campus 6",
+//     cuisine: "Burritos, Tacos, Quesadillas, Nachos",
+//     rating: "4.4",
+//     deliveryTime: "15",
+//     price: "250",
+//   },
+ 
+// ];
+
+
