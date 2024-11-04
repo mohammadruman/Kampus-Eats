26f68dbc945f63870581d319455eee1b0c4bf78b
Pratishtha Kumari <pratishtha.richu@gmail.com>
2024-11-05 00:10:16 +0530

    Auth and private routes setup

---
+++ Frontend/src/utils/PrivateRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from './AuthContext'
+
+export default function PrivateRoutes() {
+	// const {user} = useAuth()
+
+	//for development:
+	const user = true
+	return user ? <Outlet /> : <Navigate to="/login" />
+}
