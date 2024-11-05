034ff637afc99b352e59669f1738954fe890c3ee
adarsh010c <adarsh010c@gmail.com>
2024-11-05 14:20:08 +0530

    added redux persist

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,5 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import cartReducer from "./feature/cart/cartSlice"
+
+//confgure key we want to persist
+const rootPersistConfig = {
+    key: "root",
+    storage: storage,
+    whitelist: ['cart'] //only cart will be persisted
+}
+
+const rootReducer = combineReducers({
+    cart: cartReducer
+})
+
+const persistedReducer = persistReducer( rootPersistConfig, rootReducer)
 
 export const store = configureStore({
     reducer : {
