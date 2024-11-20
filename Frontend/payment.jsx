997a657d6e1ef6313c85cdbf105ab51e2628e900
Pratishtha <pratishtha.richu@gmail.com>
2024-11-21 00:50:55 +0530

    fix CORS try-1

--- Frontend/payment.jsx
+++ Frontend/payment.jsx
@@ -1,3 +1,23 @@
-export default async ({req, res, log, error}) => {
+export default async ({ req, res, log, error }) => {
+	if (req.method === 'OPTIONS') {
+		return res.send('', 200, {
+			'Access-Control-Allow-Origin': 'http://localhost:3000',
+			'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		})
+	}
+
+	if (req.method === 'POST' || req.method === 'GET') {
+		try {
+			return res.json({ ok: true }, 200, {
+				'Access-Control-Allow-Origin': 'http://localhost:3000',
+			})
+		} catch (error) {
+			return res.json({ error: 'Internal Server Error' }, 500, {
+				'Access-Control-Allow-Origin': '*',
+			})
+		}
+	}
+
 	return res.json('hello ')
 }
